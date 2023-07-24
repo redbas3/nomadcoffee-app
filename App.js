ed57987ab27520c6a7073306addbb55d37182f58
RJTmini <redbas3@gmail.com>
2023-07-24 14:00:27 +0900

    challenge#11_230724

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
+import React, { useEffect, useCallback, useState } from "react";
+
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        const fonts = loadFonts([Ionicons.font]);
+        // await Font.loadAsync(Entypo.font);
+        // await Font.loadAsync(Ionicons.font);
+
+        const images = loadImages([require("./assets/coffeeShop1.jpg")]);
+        // await Asset.loadAsync(require('./1600x800_1.jpeg'));
+        // await Image.prefetch("https://reactnative.dev/docs/assets/GettingStartedCongratulations.png")
+
+        await Promise.all([...fonts, ...images]);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View
+      onLayout={onLayoutRootView}
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    >
+      <Text>Nomad Coffee</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
